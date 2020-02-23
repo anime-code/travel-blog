5a8ee358a5135c13f48e8bb8e99f0d4bc6548b9c
vinh1679 <dstrfhxk@server226.web-hosting.com>
2020-02-23 08:24:05 -0500

    config db

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'blog_travel' );
+define( 'DB_NAME', 'dstrfhxk_blog_travel' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'dstrfhxk_blog-travel' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'vt123456' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'server226.web-hosting.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
